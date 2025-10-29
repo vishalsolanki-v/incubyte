968eff878118c130b7d77341e65e0870bc9fa6cb
vishalsolanki-v <vishalthakur2463@gmail.com>
2025-10-30 03:22:59 +0530

    few more testcases

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -1,15 +1,21 @@
 import { describe, expect, it } from "vitest";
 import { add, validateNumberString } from "./stringCalculator";
 
-describe("First Test case for Empty String",()=>{
+describe("String Calculator",()=>{
     it("should return 0 if string is empty",()=>{
         expect(add("")).toBe(0);
     })
      it("should return sum of 1,2,3",()=>{
         expect(add("1,2,3")).toBe(6);
     })
-    it("should validate string",()=>{
+    it("should validate number string for all non numeric character expect comma and space",()=>{
         expect(validateNumberString("1,2,3 3 3,3")).toBe(false)
         expect(validateNumberString("1,2,3 ?3 3,3")).toBe(true)
     })
+    it("should handle single number", () => {
+    expect(add("5")).toBe(5);
+  });
+ it("should handle large numbers", () => {
+    expect(add("100000,200000,300000")).toBe(600000);
+  });
 })
